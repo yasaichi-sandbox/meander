d2047f5ff7d0b657d15a7f7e9766e34449048f35
yasaichi <yasaichi@users.noreply.github.com>
2018-05-08 23:57:09 +0900

    7.3

---
+++ query.go
@@ -0,0 +1,41 @@
+package meander
+
+type Place struct {
+	// NOTE: You can access `Lat` by `place.Lat` because the field name for
+	// `googleGeometry` isn't specified and `Lat` is unique among the attribute
+	// names of `Place` struct
+	*googleGeometry `json:"geometry"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
+}
+
+type googleResponce struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+
+type googleLocation struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photo_reference"`
+	URL      string `json:"url"`
+}
+
+func (p *Place) Public() interface{} {
+	return map[string]interface{}{
+		"name":     p.Name,
+		"icon":     p.Icon,
+		"photos":   p.Photos,
+		"vicinity": p.Vicinity,
+		"lat":      p.Lat,
+		"lng":      p.Lng,
+	}
+}
