8504f7f1023fa53485f627f5954900f1453a7312
yasaichi <yasaichi@users.noreply.github.com>
2018-05-10 23:39:21 +0900

    7.3.2.1

--- const_level.go
+++ const_level.go
@@ -1,5 +1,9 @@
 package meander
 
+import (
+	"strings"
+)
+
 type Cost int8
 
 const (
@@ -12,3 +16,44 @@ const (
 	Cost4
 	Cost5
 )
+
+var costStrings = map[string]Cost{
+	"$":     Cost1,
+	"$$":    Cost2,
+	"$$$":   Cost3,
+	"$$$$":  Cost4,
+	"$$$$$": Cost5,
+}
+
+func (c Cost) String() string {
+	for s, v := range costStrings {
+		if v == c {
+			return s
+		}
+	}
+
+	return "不正な値です"
+}
+
+// Q. This method returns 0 if `costString` doesn't include `s`. Is this OK?
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r *CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, "...")
+
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
+}
